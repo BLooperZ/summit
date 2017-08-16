8ae4061a4f2be55a37fd58c6d8649d8a1004f7d5
nastia <anastasia.vainberg@gmail.com>
2017-08-16 15:47:31 +0300

    no react logo, seperate editor with css and buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Editor from 'components/editor'
+import './components/editor/style.css'
+import Editor from 'components/editor/edit.js'
 import TinyMCE from 'react-tinymce'
+import Buttons from 'components/editor/buttons.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+          <h2>Welcome to SummIt</h2>
         <Editor />
         <p className="App-intro">
-          To get starararted, edit <code>src/App.js</code> and save to reload.
-        </p>
 
+          Use buttons below
+
+
+        </p>
+        <Buttons/>
 
       </div>
     )
---
+++ src/components/editor/buttons.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+const Buttons = () => (
+<div className ="btns">
+<button className="btn" onClick={()=>alert("blalsd")}>Save
+    </button>
+    <button className="btn" onClick={()=>alert("blalsd")}>Post
+    </button>
+    <button className="btn" onClick={()=>alert("blalsd")}>Q&A
+    </button>
+    </div>
+)
+export default Buttons
